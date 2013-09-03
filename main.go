6c5f579b03e181626f41a7c8380faa0f7f969507
Tobias Schwab <tobias.schwab@dynport.de>
2013-09-03 12:16:25 +0200

    add method to rename servers

--- main.go
+++ main.go
@@ -1,23 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/dynport/gocli"
 	"github.com/dynport/gologger"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 )
 
-var logger = gologger.New()
-
-func main() {
-	if e := run(); e != nil {
-		logger.Error(e.Error())
-		os.Exit(1)
-	}
-}
+var logger = gologger.NewFromEnv()
 
 type Server struct {
 	Ip        string `json:"server_ip"`
@@ -53,10 +49,51 @@ func init() {
 	}
 }
 
+func (account *Account) Url() string {
+	return fmt.Sprintf("https://%s:%s@robot-ws.your-server.de", account.User, account.Password)
+}
+
+func (account *Account) RenameServer(ip string, name string) error {
+	values := url.Values{}
+	values.Add("server_name", name)
+	theUrl := account.Url() + "/server/" + ip
+	buf := bytes.Buffer{}
+	buf.WriteString(values.Encode())
+	logger.Debugf("using values %s", values.Encode())
+	req, e := http.NewRequest("POST", theUrl, &buf)
+	if e != nil {
+		return e
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	rsp, e := loadRequest(req)
+	if e != nil {
+		return e
+	}
+	defer rsp.Body.Close()
+	if strings.HasPrefix(rsp.Status, "2") {
+		logger.Infof("renamed %s to %s", ip, name)
+		return nil
+	}
+	b, e := ioutil.ReadAll(rsp.Body)
+	return fmt.Errorf("error renaming server: %s %s", rsp.Status, string(b))
+}
+
+func loadRequest(request *http.Request) (rsp *http.Response, e error) {
+	logger.Debugf("sending request: METHOD=%s, URL=%s", request.Method, request.URL.String())
+	rsp, e = (&http.Client{}).Do(request)
+	if e == nil {
+		logger.Debugf("got response %s", rsp.Status)
+	}
+	return rsp, e
+}
+
 func (account *Account) Servers() (servers []*Server, e error) {
-	url := fmt.Sprintf("https://%s:%s@robot-ws.your-server.de/server", account.User, account.Password)
+	req, e := http.NewRequest("GET", account.Url()+"/server", nil)
+	if e != nil {
+		panic("unable to create request: " + e.Error())
+	}
 	logger.Debug("fetching servers")
-	rsp, e := http.Get(url)
+	rsp, e := loadRequest(req)
 	if e != nil {
 		return servers, e
 	}
@@ -91,11 +128,19 @@ type ServerResponse struct {
 	Servers []*Server `json:"server"`
 }
 
-func run() error {
-	account, e := AccountFromEnv()
+var router = gocli.NewRouter(nil)
+var account *Account
+
+func init() {
+	var e error
+	account, e = AccountFromEnv()
 	if e != nil {
-		return e
+		logger.Error(e.Error())
+		os.Exit(1)
 	}
+}
+
+func listServers(args *gocli.Args) error {
 	servers, e := account.Servers()
 	if e != nil {
 		return e
@@ -106,4 +151,26 @@ func run() error {
 	}
 	fmt.Println(table)
 	return nil
+}
+
+func renameServer(args *gocli.Args) error {
+	if len(args.Args) != 2 {
+		return fmt.Errorf("<ip> <new_name>")
+	}
+	ip, name := args.Args[0], args.Args[1]
+	logger.Infof("renaming servers %s to %s", ip, name)
+	return account.RenameServer(ip, name)
+}
+
+func main() {
+	router.Register("servers/list", &gocli.Action{
+		Handler: listServers, Description: "list servers",
+	})
+	router.Register("servers/rename", &gocli.Action{
+		Handler: renameServer, Description: "rename server",
+	})
+	if e := router.Handle(os.Args); e != nil {
+		logger.Error(e.Error())
+		os.Exit(1)
+	}
 }
